41e11839be48a7f161f4f88762b7a944397de08b
99choco99 <99choco99@naver.com>
2025-03-24 01:52:07 +0900

    루트모션 적용, JSON 주고받기 성공, 동기화 남음

--- Assets/Scripts/Inheritance/LivingEntity.cs
+++ Assets/Scripts/Inheritance/LivingEntity.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class LivingEntity : MonoBehaviour,IDamageable
 {
@@ -46,4 +47,5 @@ public class LivingEntity : MonoBehaviour,IDamageable
         dead = false;
         currentHp = maxHp;
     }
+
 }
